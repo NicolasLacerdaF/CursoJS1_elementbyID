505315eb60b512e6975130453ec8524aca499b40
Nicolas <nicolaslacerdaa@gmail.com>
2024-07-11 23:42:51 -0300

    teste da pasta aula 2

---
+++ Aula2/index.js
@@ -0,0 +1,30 @@
+//console.log("Hello");
+//console.log("I like pizza!");
+
+
+//window.alert('This is an alert!');
+//window.alert('I like pizza!');
+
+//this is a comment// 
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza!`;
+
+/*
+ This 
+is 
+a 
+comment
+ */
+
+let x = 100;
+
+console.log(x);
+
+
+age = 25;
+
+console.log(age);
+
+
+
