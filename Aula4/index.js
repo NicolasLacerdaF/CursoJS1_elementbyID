a90e11c0b9144d0a9749c676f82252f2ef808075
Nicolas <nicolaslacerdaa@gmail.com>
2024-07-13 00:01:36 -0300

    aula4

--- Aula4/index.js
+++ Aula4/index.js
@@ -1,4 +1,25 @@
+// type conversion = change the datatype of value 
+//                  (strings, numbers, booleans)
 
 
+/* 
+Exemplo 1: 
 
+let age = window.prompt("How old are you?");
+age = Number(age);
 
+age+=1;
+
+console.log(age, typeof age);*/ 
+
+let x = "0";
+let y = "0";
+let z = "0";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
