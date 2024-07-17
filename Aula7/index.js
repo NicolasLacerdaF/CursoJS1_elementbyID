00db6a03d115f144f2f294ca3a4bdf8c51b9a7e8
Nicolas <nicolaslacerdaa@gmail.com>
2024-07-17 01:37:57 -0300

    aulas 6 7 8

---
+++ Aula7/index.js
@@ -0,0 +1,25 @@
+// math = built-in object that provides a
+//  collection of properties and methods
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+//z = Math.round(x) mais próximo;
+//z = Math.floor(x) mais baixo;
+//z = Math.ceil(x) mais alto;
+//z = Math.trunc(x) remove os valores após o ponto;
+//z = Math.pow(x , y) potência do valor base X e Y como pow;
+//z = Math.sqrt(x) raiz do valor base X e Y como raiz;
+//z = Math.log(x) logaritmo;
+//z = Math.sin(x) seno;
+//z = Math.cos(x) coseno;
+//z = Math.tan(x) tangente;
+//z = Math.abs(x) trás o valor positivo;
+//z = Math.sign(x) indicativo de sinal +1,-1 ou 0 geralmente;
+
+let max = Math.max(x ,y ,z);
+let min = Math.min(x ,y ,z);
+
+
+console.log(min);
---
+++ Aula8/index.js
@@ -0,0 +1,11 @@
+// GERADOR DE NÚMERO ALEATÓRIO
+
+
+const min = 50;
+const max = 100;
+
+let randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+console.log(randomNum);
+
+
