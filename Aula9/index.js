718ed6b9b17f407638d418bddb52eeeeda58a2e0
Nicolas <nicolaslacerdaa@gmail.com>
2024-07-17 22:51:47 -0300

    Gerador de 3 números aleatórios(dados)

--- Aula9/index.js
+++ Aula9/index.js
@@ -1,11 +1,23 @@
 // GERADOR DE NÚMERO ALEATÓRIO
 
 
-const min = 50;
-const max = 100;
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
 
-let randomNum = Math.floor(Math.random() * (max - min)) + min;
+const min = 1;
+const max = 6;
 
-console.log(randomNum);
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
-
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
