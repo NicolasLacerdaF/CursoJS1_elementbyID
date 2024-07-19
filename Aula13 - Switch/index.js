6b6317e10cd6bb932fd784a8fbe523638598afc3
Nicolas <nicolaslacerdaa@gmail.com>
2024-07-19 02:53:50 -0300

    Aula 13 parte 1 switch dias da semana

---
+++ Aula13 - Switch/index.js
@@ -0,0 +1,31 @@
+// SWITCH = pode ser uma forma eficiênte para muitas else e if's
+
+let day = 1;
+
+switch(day){
+    case 1:
+        console.log("It's Monday");
+        break;
+    case 2:
+        console.log("It's Tuesday");
+        break;
+    case 3:
+        console.log("It's Wednesday");
+        break;
+    case 4:
+        console.log("It's Thursday");
+        break;
+    case 5:
+        console.log("It's Friday");
+        break;
+    case 6:
+        console.log("It's Saturday");
+        break;
+    case 7:
+        console.log("It's Sunday");
+        break;
+    
+    default:
+    console.log(`${day} is not a day`);
+
+}
