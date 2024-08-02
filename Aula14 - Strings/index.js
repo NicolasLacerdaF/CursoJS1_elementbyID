b429cece1a987bb80db5083cd695543adf680ea9
Nicolas <nicolaslacerdaa@gmail.com>
2024-08-01 22:55:37 -0300

    aula14 strings parte 1

--- Aula14 - Strings/index.js
+++ Aula14 - Strings/index.js
@@ -1,3 +1,38 @@
 // String methods = permite que você manipule e trabalhe com textos (strings)
 
-let usaN
+
+
+//console.log(userName.charAt(0)); EMITE A PRIMEIRA LETRA
+
+//console.log(userName.lastIndexOf("o")); CONTAGEM DE TRÁS PRA FRENTE
+
+//console.log(userName.length); Quantidade de letras
+
+//userName = userName.trim(); IGN
+
+//userName = userName.toUpperCase();coloca em letra maiúcula Lower para letra minúscula
+
+//userName = userName.repeat(4);Repete em quantas vezes for adicionado no parenteses
+
+/*let userName = "Nico    las";
+
+let result = userName.includes(" "); //Avisa se o nome começa,tem ou termina com aquilo (starts,includes ou ends)
+
+console.log(result);
+
+if(result){
+    console.log("Your name can't end with '   ' ");
+}
+else{
+    console.log("Nicolas");
+}*/
+
+
+//phoneNumber = phoneNumber.replaceAll("-", ""); subtitui para o que estiver entre aspas
+
+
+//phoneNumber = phoneNumber.padStart(15, "0"); preenche com conteúdo até atingir a quantidade de caracteres(end também funciona)
+
+let phoneNumber = "123-456-7890";
+console.log(phoneNumber); 
+
