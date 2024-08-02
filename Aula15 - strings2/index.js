502c0474b2769a105c5731b3383b194e7f78761a
Nicolas <nicolaslacerdaa@gmail.com>
2024-08-02 00:10:23 -0300

    separador de itens strings
    separador de itens strings, nome sobrenome e ultimonome. E-mail e nome de usuário

---
+++ Aula15 - strings2/index.js
@@ -0,0 +1,38 @@
+// string slicing = creating a substring   
+//                    from a portion of another String
+
+//                    string.slice(start, end)
+
+const email = "nicolaslacerdaanickolichata@gmail.com";
+
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@"));
+
+console.log(username);
+console.log(extension);
+
+
+
+
+
+//let firstName = fullName.slice(0, 7);
+//let lastName = fullName.slice(8, 15);
+
+//let firstChar = fullName.slice(0, 1);
+//let lastChar = fullName.slice(-1);
+
+/***********const fullName = "Nicolas Lacerda Franco";
+
+let firstName = fullName.slice(0, fullName.indexOf(" "));
+
+let middleName = fullName.slice(fullName.indexOf(" ") + 1, fullName.lastIndexOf(" "));
+
+let lastName = fullName.slice(fullName.lastIndexOf(" ") + 1);// Extrair o nome do meio entre o primeiro e o último espaço
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);**********/
+
+
+
+
