43df69d530ef3fc769733d4cd32b72da4f7a9809
Nicolas <nicolaslacerdaa@gmail.com>
2024-08-06 02:35:27 -0300

    loop while

---
+++ Aula18 - strictequality/index.js
@@ -0,0 +1,15 @@
+// = assignment operator(operador de atribuição)
+// == comparisor operator (campara se valores são iguais)
+// === strict equality operator (campara se os valores e o tipo de dados são iguais, incluindo strings e tudo mais)
+// != inequality operator 
+// !== strict inequality operator
+
+
+const PI = "3.14";
+
+if(PI !== "3.14"){
+    console.log("That is not Pi");
+}
+else{
+    console.log("That is Pi");
+}
