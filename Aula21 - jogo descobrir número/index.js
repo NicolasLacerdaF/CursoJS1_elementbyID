70a8f693f429b8ededdc6779f536a950738f10fc
Nicolas <nicolaslacerdaa@gmail.com>
2024-08-14 00:36:43 -0300

    adivinhe o número entre 1 e 100

--- Aula21 - jogo descobrir número/index.js
+++ Aula21 - jogo descobrir número/index.js
@@ -6,4 +6,34 @@ const answer = Math.floor(Math.random() * (maxNum - minNum + 1));
 
 console.log(answer);
 
-//randomizador
+//randomizador de 1 a 100
+
+let attemps = 0;
+let guess;
+let running = true;
+
+while(running){
+
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
+    console.log(typeof guess, guess);
+
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a valid number");
+    }
+    else{
+        attemps++;
+        if(guess < answer){
+            window.alert("Too low!Try Again");
+        }
+        else if(guess > answer){
+            window.alert("Too High!Try Again");
+        }
+        else{
+            window.alert(`Correct!! The answer was ${answer}. It took you ${attemps} attemps`);
+        }
+    }
+}
